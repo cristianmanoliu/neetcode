46d0227d65ecacb390df29c1cfdcd56f9c5f7ff4
Cristian Manoliu <cristian.manoliu@gmail.com>
2025-07-24 12:35:18 +0300

    Add TwoIntegerSumII

--- neetcode-150/src/main/java/io/github/cristianmanoliu/two_pointers/TwoIntegerSumII.java
+++ neetcode-150/src/main/java/io/github/cristianmanoliu/two_pointers/TwoIntegerSumII.java
@@ -7,7 +7,7 @@ public class TwoIntegerSumII {
     int right = numbers.length - 1;
     while (left < right) {
       if (numbers[left] + numbers[right] == target) {
-        return new int[]{left + 1, right + 1}; // Return 1-based indices
+        return new int[]{left + 1, right + 1};
       } else if (numbers[left] + numbers[right] < target) {
         left++;
       } else {
