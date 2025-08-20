9904588d609fb785ae821783419a12d8900bb1ef
Cristian Manoliu <cristian.manoliu@gmail.com>
2025-08-21 00:52:51 +0300

    - Add LargestRectangleInHistogram

---
+++ neetcode-150/src/main/java/io/github/cristianmanoliu/stack/LargestRectangleInHistogram.java
@@ -0,0 +1,29 @@
+package io.github.cristianmanoliu.stack;
+
+import java.util.Stack;
+
+// https://neetcode.io/problems/largest-rectangle-in-histogram?list=neetcode150
+public class LargestRectangleInHistogram {
+  public int largestRectangleArea(int[] heights) {
+    Stack<HeightIndex> stack = new Stack<>();
+    int maxArea = 0;
+    int n = heights.length;
+
+    for (int i = 0; i <= n; i++) {
+      int currentHeight = (i == n) ? 0 : heights[i];
+      while (!stack.isEmpty() && stack.peek().height > currentHeight) {
+        HeightIndex heightIndex = stack.pop();
+        int height = heightIndex.height;
+        int width = (stack.isEmpty()) ? i : i - stack.peek().index - 1;
+        maxArea = Math.max(maxArea, height * width);
+      }
+      stack.push(new HeightIndex(currentHeight, i));
+    }
+
+    return maxArea;
+  }
+
+  private record HeightIndex(int height, int index) {
+    // This record holds the height and its index in the histogram.
+  }
+}
---
+++ neetcode-150/src/test/java/io/github/cristianmanoliu/stack/LargestRectangleInHistogramTest.java
@@ -0,0 +1,26 @@
+package io.github.cristianmanoliu.stack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LargestRectangleInHistogramTest {
+
+  private final LargestRectangleInHistogram largestRectangleInHistogram = new LargestRectangleInHistogram();
+
+  @Test
+  void example1_largestRectangleArea_returns8() {
+    int[] heights = {7, 1, 7, 2, 2, 4};
+    int expected = 8;
+    int result = largestRectangleInHistogram.largestRectangleArea(heights);
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void example2_largestRectangleArea_returns7() {
+    int[] heights = {1, 3, 7};
+    int expected = 7;
+    int result = largestRectangleInHistogram.largestRectangleArea(heights);
+    assertEquals(expected, result);
+  }
+}
