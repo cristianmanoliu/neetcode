3056c94a3fec5c01632daa2c9087e73fc66d96e7
Cristian Manoliu <cristian.manoliu@gmail.com>
2025-08-23 09:54:15 +0300

    - Add SearchA2DMatrix

---
+++ neetcode-150/src/main/java/io/github/cristianmanoliu/binary_search/SearchA2DMatrix.java
@@ -0,0 +1,37 @@
+package io.github.cristianmanoliu.binary_search;
+
+// https://neetcode.io/problems/search-2d-matrix?list=neetcode150
+public class SearchA2DMatrix {
+  public boolean searchMatrix(int[][] matrix, int target) {
+    int top = 0;
+    int bottom = matrix.length - 1;
+    while (top <= bottom) {
+      int midRow = top + (bottom - top) / 2;
+      if (matrix[midRow][0] <= target && matrix[midRow][matrix[0].length - 1] >= target) {
+        return search(matrix[midRow], target) != -1;
+      } else if (matrix[midRow][0] < target) {
+        top = midRow + 1;
+      } else {
+        bottom = midRow - 1;
+      }
+    }
+
+    return false;
+  }
+
+  public int search(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return -1;
+  }
+}
---
+++ neetcode-150/src/test/java/io/github/cristianmanoliu/binary_search/SearchA2DMatrixTest.java
@@ -0,0 +1,36 @@
+package io.github.cristianmanoliu.binary_search;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SearchA2DMatrixTest {
+
+  private final SearchA2DMatrix searchA2DMatrix = new SearchA2DMatrix();
+
+  @Test
+  void example1_searchMatrix_returnsTrue() {
+    int[][] matrix = {
+        {1, 2, 4, 8},
+        {10, 11, 12, 13},
+        {14, 20, 30, 40}
+    };
+    int target = 10;
+    boolean expected = true;
+    boolean result = searchA2DMatrix.searchMatrix(matrix, target);
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void example2_searchMatrix_returnsFalse() {
+    int[][] matrix = {
+        {1, 2, 4, 8},
+        {10, 11, 12, 13},
+        {14, 20, 30, 40}
+    };
+    int target = 15;
+    boolean expected = false;
+    boolean result = searchA2DMatrix.searchMatrix(matrix, target);
+    assertEquals(expected, result);
+  }
+}
