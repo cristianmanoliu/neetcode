85f6f0a3725c73cefc9f5c63e606f8635e3499f9
Cristian Manoliu <cristian.manoliu@gmail.com>
2025-08-27 21:14:22 +0300

    - Add KokoEatingBananas

---
+++ neetcode-150/src/test/java/io/github/cristianmanoliu/binary_search/KokoEatingBananasTest.java
@@ -0,0 +1,28 @@
+package io.github.cristianmanoliu.binary_search;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class KokoEatingBananasTest {
+
+  private final KokoEatingBananas kokoEatingBananas = new KokoEatingBananas();
+
+  @Test
+  void example1_minEatingSpeed_returns2() {
+    int[] piles = {1, 4, 3, 2};
+    int h = 9;
+    int expected = 2;
+    int result = kokoEatingBananas.minEatingSpeed(piles, h);
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void example2_minEatingSpeed_returns25() {
+    int[] piles = {25, 10, 23, 4};
+    int h = 4;
+    int expected = 25;
+    int result = kokoEatingBananas.minEatingSpeed(piles, h);
+    assertEquals(expected, result);
+  }
+}
